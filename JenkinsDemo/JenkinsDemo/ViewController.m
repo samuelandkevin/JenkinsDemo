716b674789332f22232dc32c8b3c52dcf808201e
huangkunpeng <huangkunpeng@ilegend.cn>
2019-09-15 11:40:04 +0800

    first commit

---
+++ JenkinsDemo/JenkinsDemo/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  JenkinsDemo
+//
+//  Created by 黄坤鹏 on 2019/9/15.
+//  Copyright © 2019 黄坤鹏. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    NSLog(@"JenkinsDemo");
+}
+
+
+@end
---
+++ JenkinsDemo/JenkinsDemoTests/JenkinsDemoTests.m
@@ -0,0 +1,37 @@
+//
+//  JenkinsDemoTests.m
+//  JenkinsDemoTests
+//
+//  Created by 黄坤鹏 on 2019/9/15.
+//  Copyright © 2019 黄坤鹏. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JenkinsDemoTests : XCTestCase
+
+@end
+
+@implementation JenkinsDemoTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
